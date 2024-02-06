de8a3b1ce0f8cb57f301ecd9612092a4d3218741
knight433 <dhruvanu3@gmail.com>
2024-02-06 17:10:37 +0530

    Co-authored-by: 1rn21ai071.mayurgowda@gmail.com <1rn21ai071.mayurgowda@gmail.com>

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,23 +1,11 @@
 import numpy as np
 
-class InputLayer:
-
-    def __init__(self, number_of_input): 
-        self.number_of_input = number_of_input
+class layersT:
     
-    #to add the vlaues in the network 
-    def use(self, input_matrix):
-        if isinstance(input_matrix,np.ndarray):
-            return input_matrix
-        else:
-            self.out_matrix = np.array(input_matrix)
-            return self.out_matrix
-
-class HiddenLayer:
-    def __init__(self, number_of_input, number_of_neurons, activation='relu'):
+    def __init__(self, number_of_input, number_of_neurons,activation='relu',wig=None,bia=None):
         low, high = -1, 1
-        self.weight_matrix = np.random.uniform(low, high, size=(number_of_input, number_of_neurons))
-        self.bias = np.random.uniform(low, high, size=(1, number_of_neurons))
+        self.weight_matrix = wig
+        self.bias = bia
         self.activation = activation
 
     def apply_activation(self, values):
@@ -27,70 +15,85 @@ class HiddenLayer:
             return 1 / (1 + np.exp(-values))
         elif self.activation == 'tanh':
             return np.tanh(values)
+        elif self.activation == 'softmax':
+            e_x = np.exp(values - np.max(values, axis=-1, keepdims=True))
+            return e_x / np.sum(e_x, axis=-1, keepdims=True)
         else:
             return values  # No activation (identity)
 
+    def compute_derivative(self, values):
+        if self.activation == 'relu':
+            return (values > 0).astype(float)
+        elif self.activation == 'sigmoid':
+            sigmoid_output = self.apply_activation(values)
+            return sigmoid_output * (1 - sigmoid_output)
+        elif self.activation == 'tanh':
+            tanh_output = self.apply_activation(values)
+            return 1 - tanh_output**2
+        elif self.activation == 'softmax':
+            softmax_output = self.apply_activation(values)
+            return softmax_output * (1 - softmax_output)  # Note: This derivative might not be used often for softmax
+        else:
+            return np.ones_like(values)  # Derivative of the identity function is 1
+
     def forward(self, input_matrix):
         values = np.dot(input_matrix, self.weight_matrix) + self.bias
-        return self.apply_activation(values)
+        self.neuronValues =  self.apply_activation(values)
+        #print(f'r =  {self.neuronValues} values = {values}') #debugging
+        return self.neuronValues
+    
+wig1 = np.array([[-0.80377147, -0.63344107],
+                 [0.43278832, -0.02537794],
+                 [-0.71144554, 0.79602224]])
+wig2 = np.array([[ 0.30313998,-0.651056],
+                 [ 0.11990958, -0.68028781]])
+wig3 = np.array([[-0.13948543, -0.15244189 ,0.02360036],
+                 [ 0.15260582 ,-0.41243941 ,0.62087499]])
 
+b1 = np.array([-0.20960194,-0.86772309])
+b2 = np.array([-0.04218223, 0.71097538])
+b3 = np.array([-0.45757658, -0.6612067, 0.39908153])
 
-class Model:
-   
-    def __init__(self):
-        self.inputNumber = input('Enter the number of input paramentes') # input nodes
+testInputs = [3,2,'relu',wig1,b1,2,2,'relu',wig2,b2,2,3,'softmax',wig3,b3]
+
+class ModelTest:
+
+    #to intiallize the model
+    def __init__(self) -> None:
+        
+        inputNumber = 3 # input nodes
         self.numberOfLayers = 3 #number of hidden layer and output layer
 
         self.listOfHiddenLayers = []
-        self.inLayerObj = InputLayer(self.inputNumber)
-
+        self.inLayerObj = inputLayer(inputNumber)
+        ind = 0
         for i in range(self.numberOfLayers):
-            number_of_input = int(input('number of input: '))
-            numberOfNeurons = int(input('number of neurons: '))
-            activation = input('Enter the activation funtion: ')
-            hidden_layer_obj = HiddenLayer(number_of_input,numberOfNeurons,activation)
+            number_of_input = testInputs[ind]
+            ind += 1
+            numberOfNeurons = testInputs[ind]
+            ind += 1
+            activation = testInputs[ind]
+            ind += 1
+            w = testInputs[ind]
+            ind += 1
+            b = testInputs[ind]
+            ind += 1
+            hidden_layer_obj = layersT(number_of_input,numberOfNeurons,activation,w,b)
 
             self.listOfHiddenLayers.append(hidden_layer_obj)
-    
-    #to get cost funtion of the layer 
-    def costFuntion(self,value_matrix,req_arr,):
-        
-        cost_funtion = np.subtract(value_matrix,req_arr)**2
-
-        return cost_funtion
-
-    #pridict while training
-    def train_pridict(self,features):
-        
-        arr = self.inLayerObj.use(features)
-
-        for layer in self.listOfHiddenLayers:
-            arr = layer.forward(arr)
-        
-        return arr
-    
-    def magic(self,value_matrix,label,layerSize):
-        pass
-
-    #to train the data
-    def train(self,data):
-        
-        n = len(data[0]) - 2 
-
-        for row in data:
             
-            label = row[0]
-            features = row[1:n]
-        
-        temp_arr = self.train_pridict(features)
-        
+            if i == self.numberOfLayers-1:
+                self.numberOfLabels = numberOfNeurons
 
-    def pridict(self,data):
+    def pridict(self,data,ForTraining = False):
 
         arr = self.inLayerObj.use(data)
         
         for layer in self.listOfHiddenLayers:
             arr = layer.forward(arr)
+
+        if ForTraining:
+            return arr
 
         pridiction_value = -1
         pridiction = None
@@ -100,3 +103,43 @@ class Model:
                 pridiction = {'label' : i, 'confidence' : obj}
         
         return pridiction
+    
+
+    def view_weights(self):
+        for layer in self.listOfHiddenLayers:
+            print(f'weights \n - {layer.weight_matrix}')
+            print(f'bias - {layer.bias}')
+
+    #backpropagation 
+    def train(self,data,label,learningRate=0.1):
+        
+        pridiction = self.pridict(data,ForTraining = True)
+        reqArr =np.array([0 for i in range(self.numberOfLabels)])
+        reqArr[label] = 1
+        costFuntion = sum((pridiction - reqArr)**2)
+        
+        # print(reqArr)
+        # print(costFuntion)
+        # print(f"prid - {pridiction}")
+
+        pridiction = pridiction - reqArr
+        i = self.numberOfLayers - 1
+
+        while i:
+            print(i)
+            layers = self.listOfHiddenLayers[i]
+            gradient = np.array([np.multiply(pridiction,layers.compute_derivative(pridiction))])
+            print(gradient)
+            a = np.array([self.listOfHiddenLayers[i-1].neuronValues])
+            print(a)
+            pr = layers.weight_matrix.T - learningRate*(np.dot(gradient.T,a))
+            self.listOfHiddenLayers[i].weight_matrix = pr.T
+            i -= 1
+
+        layers = self.listOfHiddenLayers[0]
+        gradient = np.array([np.multiply(pridiction,layers.compute_derivative(pridiction))])
+        
+        a = np.array([data])
+        pr = layers.weight_matrix.T - learningRate*(np.dot(gradient.T,a))
+        self.listOfHiddenLayers[0].weight_matrix = pr.T
+
